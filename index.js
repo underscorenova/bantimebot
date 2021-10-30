b34996b5ba3325cd107c9b00903ce7cb227a01cb
underscorenova <underscorenova@gmail.com>
2021-10-30 13:23:11 +0200

    ?

--- index.js
+++ index.js
@@ -113,3 +113,10 @@ client.on('interactionCreate', async (interaction) => {
         });
     }
 });
+
+const http = require('http');
+const server = http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end('ok');
+});
+server.listen(3000);
